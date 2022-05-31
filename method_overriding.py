0fa772adff22379c6f11206ee471c37f132d191a
shainaz1207 <105064443+shainaz1207@users.noreply.github.com>
2022-05-31 23:49:37 +0530

    Create method_overriding.py

---
+++ method_overriding.py
@@ -0,0 +1,12 @@
+class parent():
+    def study(self):
+        print("Keep Studying")
+
+class child(parent):
+    def study(self):
+        print('Take a break')
+
+c=child()
+c.study()
+
+# child.study()
